33a1361b2570e734c542776a4f6214edff0155ee
Angelo <atan958@aucklanduni.ac.nz>
2022-06-05 02:49:27 +1200

    Added the submission files to the repo.

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+/* Modal */
 const modal = document.getElementById("modal");
 const modalContent = document.getElementById("modal-content");
 
+/*
+*   Opens the modal
+*/
 const openModal = () => {
     modal.style.transition= "0.5s ease";
     modal.style.zIndex = "5";
@@ -9,6 +13,9 @@ const openModal = () => {
     modalContent.style.margin = "10vh auto auto auto";
 }
 
+/*
+*   Closes the modal
+*/
 const closeModal = () => {
     modal.style.transition= "1s ease";
     modal.style.zIndex = "-5";
@@ -17,9 +24,32 @@ const closeModal = () => {
     modalContent.style.margin = "0vh auto auto auto";
 }
 
+/* Registration Form Input */ 
+const userInputs = [];
 
-const formSubmit = document.getElementById("form-submit");
+/*
+*   Groups corresponding label and input elements to an object
+*/
+for(i=0; i<8; i++) {
+    const inputId = `input-${i}`;
+    const labelId = `input-label-${i}`;
 
-formSubmit.onSubmit = (e) => {
-    e.preventDefault();
+    const input = document.getElementById(inputId);
+    const label = document.getElementById(labelId);
+    
+    userInputs.push({input,label});
 }
+
+/*
+*   Turns on/off the highlighting of the input labels
+*/
+userInputs.forEach((userInput) => {
+    userInput.input.addEventListener("focus",() => {
+        userInput.label.style.color = "rgb(214, 70, 62)";
+    });
+    userInput.input.addEventListener("blur",() => {
+        userInput.label.style.color = "";
+    });
+});
+
+
